b6d0b3628075f68c6106ee814e8deef836e9a626
xues <xues@91wangjing.com>
2018-01-30 17:30:45 +0800

    Spring boot properties

--- src/main/java/com/xue/Springboot01Application.java
+++ src/main/java/com/xue/Springboot01Application.java
@@ -9,6 +9,4 @@ public class Springboot01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot01Application.class, args);
 	}
-	
-	
 }
--- src/main/java/com/xue/controller/IndexController.java
+++ src/main/java/com/xue/controller/IndexController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +14,37 @@ import com.xue.bean.User;
 @RequestMapping(value="/index")
 @RestController
 public class IndexController {
-
+	
+	@Value(value="${xue.secret}")
+	private String secret;
+	@Value(value="${xue.number}")
+	private Integer number;
+	@Value(value="${xue.bignumber}")
+	private String bignumber;
+	@Value(value="${xue.number.less.than.ten}")
+	private Integer less;
+	@Value(value="${xue.number.in.range}")
+	private String range;
+	@Value(value="${xue.helloworld}")
+	private String hello;
+	@Value(value="${xue.user}")
+	private String user;
 	@RequestMapping
 	public String index() {
 		return "hello world";
 	}
 
 	@RequestMapping(value="/get")
-	public Map<String,String> getMap(){
-		Map<String,String> map = new HashMap<String,String>();
+	public Map<String,Object> getMap(){
+		Map<String,Object> map = new HashMap<String,Object>();
 		map.put("name", "hello");
+		map.put("secret",secret);
+		map.put("number", number);
+		map.put("bignumber", bignumber);
+		map.put("less", less);
+		map.put("range", range);
+		map.put("hello", hello);
+		map.put("user", user);
 		return map;
 	}
 	
